783a25d4308caf576b1ea2d4a97b4581ba2326b8
smartdev007 <kevadiajenish7@gmail.com>
2021-07-09 21:23:02 -0400

    Initial Commit

---
+++ src/components/Map.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+import GoogleMapReact from "google-map-react";
+import LocationMarker from "./LocationMarker";
+import LocationInfoBox from "./LocationInfoBox";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const WILDFIRE = 8;
+
+const Map = ({ eventData, center, zoom}) => {
+    const [locationInfo, setLocationInfo] = useState(null);
+
+    const markers = eventData.map(ev => {
+        if(ev.categories[0].id === WILDFIRE) {
+            return <LocationMarker 
+                        key={ev.id}
+                        lat={ev.geometries[0].coordinates[1]} 
+                        lng={ev.geometries[0].coordinates[0]} 
+                        onClick={() => setLocationInfo({ id: ev.id, title: ev.title })}
+                    />
+        }
+    })
+
+    return (
+        <div className="map">
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: API_KEY }}
+                defaultCenter={ center }
+                defaultZoom={ zoom }
+            >
+                {markers}
+            </GoogleMapReact>
+
+            {locationInfo && <LocationInfoBox info={locationInfo} />}
+        </div>
+    )
+}
+
+Map.defaultProps = {
+    center: {
+        lat: 42.3265,
+        lng: -122.8756
+    },
+    zoom: 6
+}
+
+export default Map
